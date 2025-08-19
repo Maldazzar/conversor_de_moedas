15ee9b846935a3dc4052d78b565d30877d1a3543
Maldazzar <lord.maldazzar@gmail.com>
2025-08-18 21:57:35 -0300

    algumas mudanças

--- src/ConversaoDeValor.java
+++ src/ConversaoDeValor.java
@@ -8,7 +8,7 @@ import java.net.http.HttpResponse;
 
 public class ConversaoDeValor {
     //private double valor;
-    private static final String API_KEY = "b351e38d3994105c3c12fa13";
+    private static final String API_KEY = "cbb4f40e4bb6d76a7f0f2a4d";
 
     public String converter(String moedaOrigem, String moedaDestino, double valor) {
         String url = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/" + moedaOrigem;
@@ -22,15 +22,12 @@ public class ConversaoDeValor {
             HttpResponse<String> response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
             String json = response.body();
-            //System.out.println(json);
-
-            //Faz o parsing de JSON para objeto moeda
             MoedaResponse moeda = new Gson().fromJson(json, MoedaResponse.class);
+            if (moeda.conversion_rates() == null || !moeda.conversion_rates().containsKey(moedaDestino)) {
+            return "Erro: código de moeda inválido ou não suportado.";
+            }
             double taxa = moeda.conversion_rates().get(moedaDestino);
-            //System.out.println(usd);
             double valorConvertido = valor * taxa;
-
-            //String resultado = "O valor R$ " + valor + " convertido para dolar é $ " + valorConvertido;
             return String.format("O valor %.2f %s convertido para %s é de $ %.2f", valor, moedaOrigem, moedaDestino, valorConvertido);
 
         } catch (IOException | InterruptedException e) {
